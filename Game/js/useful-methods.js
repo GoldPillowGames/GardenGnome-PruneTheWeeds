68445788ce48789a186328ed3156c1b2308c3d35
Ignacio Atance Loras <ignacio.atance.loras@gmail.com>
2020-11-19 19:17:31 +0100

    Update useful-methods.js

--- Game/js/useful-methods.js
+++ Game/js/useful-methods.js
@@ -10,7 +10,7 @@
  */
 
 export default class UsefulMethods{
-    static isDebug = true;
+    static isDebug = false;
 
     static vectorModule(x, y) {
         return Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
